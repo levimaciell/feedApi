8f81c649ac877c7d705ebb08c12f80fc0929fa33
= <=>
2023-09-04 09:49:59 -0300

    setup

---
+++ demo/src/main/java/com/grupofive/demo/FeedApiApplication.java
@@ -0,0 +1,13 @@
+package com.grupofive.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedApiApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/grupofive/demo/FeedApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupofive.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
