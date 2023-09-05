a88235a368d602cafc3b24dc99100ab077dd5a54
levimaciell <levimaciell1221@gmail.com>
2023-09-05 16:12:49 -0300

    Setup, application.properties and post entity

--- demo/src/main/java/com/grupofive/demo/post/entities/Post.java
+++ demo/src/main/java/com/grupofive/demo/post/entities/Post.java
@@ -1,5 +1,47 @@
 package com.grupofive.demo.post.entities;
 
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_posts")
 public class Post {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(columnDefinition = "TEXT")
+    private String message;
+
+    public Post(){
+
+    }
+
+    public Post(UUID id, String message){
+        this.id = id;
+        this.message = message;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
