a5cb682f71b622be1382acfdbe3d2ca18bc89a18
SilvioNascimento <silvio.estudante19@gmail.com>
2023-11-04 13:22:23 -0300

    Adicionando colunas para a tabela tb_posts

--- demo/src/main/resources/db/migration/V1__create-table-tb_posts.sql
+++ demo/src/main/resources/db/migration/V1__create-table-tb_posts.sql
@@ -2,3 +2,9 @@ CREATE TABLE tb_posts (
     id TEXT PRIMARY KEY UNIQUE NOT NULL,
     message TEXT NOT NULL
 )
+
+ALTER TABLE tb_posts
+ADD COLUMN user_id TEXT NOT NULL;
+
+ALTER TABLE tb_posts
+ADD FOREIGN KEY (user_id) REFERENCES tb_users(id);
