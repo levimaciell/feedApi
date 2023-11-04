610ccad6cf684f79df03ecb01e127670311a0d3c
SilvioNascimento <silvio.estudante19@gmail.com>
2023-11-04 13:22:35 -0300

    Adicionando colunas para a tabela tb_Comments

--- demo/src/main/resources/db/migration/V2__create-table-tb_Comments.sql
+++ demo/src/main/resources/db/migration/V2__create-table-tb_Comments.sql
@@ -2,3 +2,24 @@ CREATE TABLE tb_Comments (
     commentId TEXT PRIMARY KEY UNIQUE NOT NULL,
     comment TEXT NOT NULL
 )
+
+-----------------------------------------------------------
+
+ALTER TABLE tb_Comments
+ADD COLUMN post_id TEXT NOT NULL;
+
+ALTER TABLE tb_Comments
+ADD FOREIGN KEY (post_id) REFERENCES tb_posts(id);
+
+-----------------------------------------------------------
+
+ALTER TABLE tb_Comments
+ADD COLUMN user_id TEXT NOT NULL;
+
+ALTER TABLE tb_Comments
+ADD FOREIGN KEY (user_id) REFERENCES tb_users(id)
+
+----------------------------------------------------------
+
+ALTER TABLE tb_Comments
+DROP COLUMN id
